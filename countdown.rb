281f87fdc54b983dfe47f438c6931251a0cc9e71
paulleerentveld <paul@veldnet.com.au>
2022-06-12 11:44:29 +1000

    lab done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+#countdown(10)
+
+def countdown_with_sleep(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    return puts "HAPPY NEW YEAR!"
+
+end
+
+#countdown_with_sleep(10)
